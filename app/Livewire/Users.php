8feed5e87e89bb04c515cfca310417fd658c11ad
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2025-05-14 22:07:24 +0200

    #2 sweetlert2

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,34 +3,107 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 use Livewire\Attributes\Rule;
+use Illuminate\Support\Facades\Session;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 
 class Users extends Component
 {
-    #[Rule('required|string')]
     public $name = "";
-
-    #[Rule('required|email|unique:users,email')]
     public $email = "";
-
-    #[Rule(['required','string'])]
     public $password = "";
+
+
+    public function SaveUserConfirm(){
+        LivewireAlert::title('Save User?')
+            ->text('Are You Sure?')
+            ->question()
+            ->withConfirmButton('Save')
+            ->onConfirm('SaveUser')
+            ->withCancelButton('Cancel')
+            ->onDismiss('canceled')
+            ->timer(9000)
+            ->show();
+    }
+
+    public function canceled(){
+        LivewireAlert::title('canceled')
+            ->info()
+            ->position('center')
+            ->toast()
+            ->show();
+    }
 
     public function SaveUser(){
 
-        // $data = $this->validate([
-        //     'name'=>'required|string',
-        //     'email'=>'required|email|unique:users,email',
-        //     'password'=>'required|string',
-        // ]);
+        $data = $this->validate([
+            'name'=>'required|string',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|string',
+        ]);
+        try{
 
-        $data = $this->validate();
+            DB::beginTransaction();
 
-        User::create($data);
+            User::create($data);
+            // Session::flash('success','User Created Successfully');
+            LivewireAlert::title('User Created!')
+                ->text('User Created Successfully.')
+                ->success()
+                ->show();
 
-        // $this->reset();
+
+            DB::commit();
+
+            $this->reset();
+        }
+            catch (\Exception $e) {
+            DB::rollBack();
+
+            LivewireAlert::title('Error')
+                ->text('Failed to create user. Please try again.' . $e->getMessage())
+                ->error()
+                ->position('center')
+                ->toast()
+                ->show();
+        }
+    }
+
+    public $confirmationText = '';
+    public $deleteUserId = null;
+
+
+
+
+    public function DeleteUser()
+    {
+            $this->validate([
+                    'confirmationText' => ['string','in:Delete','required'],
+                    'deleteUserId' => ['required','exists:users,id'],
+                ]);
+
+
+            $user = User::findOrFail($this->deleteUserId);
+            $user->delete();
+
+            $this->deleteUserId = null;
+            $this->confirmationText = '';
+
+            // Session::flash('success','User Deleted Successfully');
+
+            LivewireAlert::title('Success')
+                ->text('User deleted successfully!')
+                ->success()
+                ->position('center')
+                ->toast()
+                ->show();
+
+            $this->modal('delete-user-modal')->close();
+
+
     }
 
     public function render()
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form>
+    <form >
         <div class="space-y-6 w-2xl mx-auto">
             <flux:field>
                 <flux:label>name</flux:label>
@@ -19,12 +19,22 @@
                 <flux:error name="password" />
             </flux:field>
 
-            <flux:button wire:click='SaveUser' variant="primary">Save</flux:button>
+            {{-- Save with wire:confirm.prompt --}}
+            {{-- <flux:button wire:confirm='Are you Sure ?' wire:click='SaveUser' variant="primary">Save</flux:button> --}}
+
+            {{-- Save with confirm with SweetAlert --}}
+            <flux:button wire:click='SaveUserConfirm' variant="primary">Save</flux:button>
         </div>
     </form>
 
+    @if(session('success'))
+        <div class="w-2xl mx-auto p-4 my-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-green-700 dark:text-green-400" >
+            <span class="font-medium">{{ session('success') }}</span>
+        </div>
+    @endif
+
     <div
-        class="mt-5 overflow-hidden w-full overflow-x-auto rounded-sm border border-neutral-300 dark:border-neutral-700">
+        class="mt-5 w-2xl overflow-hidden mx-auto overflow-x-auto rounded-sm border border-neutral-300 dark:border-neutral-700">
         <table class="w-full text-left text-sm text-neutral-600 dark:text-neutral-300">
             <thead
                 class="border-b border-neutral-300 bg-neutral-50 text-sm text-neutral-900 dark:border-neutral-700 dark:bg-neutral-900 dark:text-white">
@@ -32,17 +42,56 @@
                     <th scope="col" class="p-4">Id</th>
                     <th scope="col" class="p-4">Name</th>
                     <th scope="col" class="p-4">Email</th>
+                    <th scope="col" class="p-4">Actions</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-neutral-300 dark:divide-neutral-700">
                 @foreach ($users as $user)
-                <tr class="even:bg-black/5 dark:even:bg-white/10">
+                <tr class="even:bg-black/5 dark:even:bg-white/10" wire:key='{{ $user->id }}'>
                     <td class="p-4">{{ $user->id }}</td>
                     <td class="p-4">{{ $user->name }}</td>
                     <td class="p-4">{{ $user->email }}</td>
+                    <td class="p-4">
+                        {{-- Delete with wire:confirm.prompt --}}
+                        {{-- <flux:button wire:confirm.prompt="Are you sure?\n\nType Delete to confirm|Delete" variant="danger" wire:click='DeleteUser' x-on:click="$wire.deleteUserId={{ $user->id }}" size="sm">Delete</flux:button> --}}
+
+                        {{-- Delete with modal Confirmation --}}
+                        <flux:modal.trigger name="delete-user-modal">
+                            <flux:button variant="danger" x-on:click="$wire.deleteUserId={{ $user->id }}" size="sm">Delete</flux:button>
+                        </flux:modal.trigger>
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+
+    <!-- Single Delete Confirmation Modal -->
+    <flux:modal name="delete-user-modal" class="md:w-96">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Confirm Deletion</flux:heading>
+                <flux:text class="mt-2">Are you sure you want to delete this user?</flux:text>
+            </div>
+
+            <!-- Confirmation Text Input -->
+            <flux:field>
+                <flux:label>Type 'Delete' to confirm</flux:label>
+                <flux:input type="text" wire:model='confirmationText' />
+                <flux:error name="confirmationText" />
+            </flux:field>
+
+            <div class="flex gap-2">
+
+                <!-- Delete Button -->
+                <flux:button wire:click="DeleteUser" variant="danger">Delete
+                </flux:button>
+                <!-- Cancel Button -->
+                <flux:modal.close>
+                    <flux:button variant="ghost" x-on:click="$wire.confirmationText = '' ">Cancel</flux:button>
+                </flux:modal.close>
+            </div>
+
+        </div>
+    </flux:modal>
 </div>
